3d6e134c11153c606566fad9a844ba0843788557
EsssW <43808999+EsssW@users.noreply.github.com>
2021-09-08 13:32:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+
+namespace HW_2_TA_FIB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger  t1 = 1, t2 = 1, t3 = 0; ;
+            int desired_fibo_number = 1000;
+
+            for (int i = 3; i <= desired_fibo_number; i++)
+            {
+                t3 = t1 + t2;
+                t1 = t2; t2 = t3;
+            }
+            Console.WriteLine($"{desired_fibo_number}-e число фибоначчи= \n{t3}");
+        }
+    }
+}
